1836bd3f2581f78482391a9f7afc9b8773746b4f
jeeneee <woojin7124@naver.com>
2020-10-23 21:25:51 +0900

    Add naver SEO

--- src/html.js
+++ src/html.js
@@ -9,6 +9,10 @@ export default class HTML extends React.Component {
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <meta
+            name="naver-site-verification"
+            content="86c926aae0ebb2ff5738851d5ff63b97e7eba0fc"
+          />
+          <meta
             name="viewport"
             content="width=device-width, initial-scale=1, shrink-to-fit=no, minimum-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover"
           />
